7211f4351f264dcc8cb7d8ce37aa9e952da49705
OdetaJur <149832504+OdetaJur@users.noreply.github.com>
2024-01-24 09:33:33 +0200

    Update SavarankiskasDarbas1.cpp

--- SavarankiskasDarbas1.cpp
+++ SavarankiskasDarbas1.cpp
@@ -3,11 +3,12 @@
 int main()
 {
 	vector<Student> Grupe;
+	string filename;
 
-	for (int i = 0; i < 3; i++) {
-		Student Laikinas;
-		string laikS;
-		vector<int> Vec;
+	//for (int i = 0; i < 3; i++) {
+		//Student Laikinas;
+		//string laikS;
+		/*vector<int> Vec;
 		cout << " Iveskite varda: ";
 		cin >> laikS; Laikinas.SetName(laikS);
 
@@ -25,12 +26,19 @@ int main()
 		cout << "Koks egzamino pazymys? ";
 		cin >> ndNr;
 		Laikinas.SetExam(ndNr);
-		Laikinas.Rezult('v');
+		Laikinas.Rezult('v');*/
 		//cin >> Laikinas;
-		Grupe.push_back(Laikinas);
-		Laikinas.~Student();
-	}
-	for (auto &duom : Grupe) duom.printas();
+		//Grupe.push_back(Laikinas);
+		//Laikinas.~Student();
+	//}
+	//for (auto &duom : Grupe) duom.printas();
+
+	cout << "Iveskite failo pavadinima" << endl;
+	cin >> filename;
+
+	SkaitytiFaila(Grupe, filename);
+
+
 
 	system("pause");
 }
