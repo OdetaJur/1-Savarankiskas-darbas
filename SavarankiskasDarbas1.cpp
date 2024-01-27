ce1344a8737f88b5b2c6f0595e990f1b8474d268
OdetaJur <149832504+OdetaJur@users.noreply.github.com>
2024-01-27 14:33:13 +0200

    Update SavarankiskasDarbas1.cpp

--- SavarankiskasDarbas1.cpp
+++ SavarankiskasDarbas1.cpp
@@ -3,43 +3,44 @@
 int main()
 {
 	vector<Student> Grupe;
-	string filename;
+	string failo_vardas;
 
 	//for (int i = 0; i < 3; i++) {
-		//Student Laikinas;
-		//string laikS;
-		/*vector<int> Vec;
-		cout << " Iveskite varda: ";
-		cin >> laikS; Laikinas.SetName(laikS);
+		Student Laikinas;
+	//	string laikS;
+	//	vector<int> Vec;
+	////	cout << " Iveskite varda: ";
+	////	cin >> laikS; Laikinas.SetName(laikS); // perkeliam į laikinas
 
-		cout << " Iveskite pavarde: ";
-		cin >> laikS; Laikinas.SetSurname(laikS);
+	//	cout << " Iveskite pavarde: ";
+	//	cin >> laikS; Laikinas.SetSurname(laikS); // perkeliam į laikinas
 
-		cout << " Kiek namu darbu buvo semestre? ";
-		int ndNr; cin >> ndNr;
-		for (int i = 0; i < ndNr; i++) {
-			int pazym;
-			cout << "Iveskite " << i + 1 << " Pazymiai: ";
-			cin >> pazym; Vec.push_back(pazym);
-		}
-		Laikinas.SetHW(Vec); Vec.clear();
-		cout << "Koks egzamino pazymys? ";
-		cin >> ndNr;
-		Laikinas.SetExam(ndNr);
-		Laikinas.Rezult('v');*/
-		//cin >> Laikinas;
-		//Grupe.push_back(Laikinas);
-		//Laikinas.~Student();
+	//	cout << " Kiek namu darbu buvo semestre? ";
+	//	int ndNr; cin >> ndNr;
+	//	for (int i = 0; i < ndNr; i++) {
+	//		int pazym;
+	//		cout << "Iveskite " << i + 1 << " pazymi: ";
+	//		cin >> pazym; Vec.push_back(pazym); // perkeliam į vec
+	//	}
+	//	Laikinas.SetHW(Vec); Vec.clear(); // perkeliam į laikiną ; išvalom Vec
+	//	cout << "Koks egzamino pazymys? ";
+	//	cin >> ndNr;
+	//	Laikinas.SetExam(ndNr); //
+	//	Laikinas.Rezult('v');
+	//	//cin >> Laikinas;
+	//	Grupe.push_back(Laikinas); // perkeliam iš laikino į grupe
+	//	Laikinas.~Student(); // išvalom laikiną su destruktorium
 	//}
-	//for (auto &duom : Grupe) duom.printas();
-
-	cout << "Iveskite failo pavadinima" << endl;
-	cin >> filename;
-
-	SkaitytiFaila(Grupe, filename);
+	//for (auto& duom : Grupe) duom.printas();
 
 
-
+	////////////////////////////////////////////////////////
+	///failo nuskaitymas
+	cout << "Įveskite pilną failo vardą" << endl;
+	cin >> failo_vardas;
+	nuskait(Grupe, failo_vardas);
+	for (auto& duom : Grupe) duom.printas()
+		
 	system("pause");
 }
 
